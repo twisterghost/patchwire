968ca0c6f3abc08423a4c4ea72730fb6c2c88917
Michael Barrett <michaelbarrett@hey.com>
2020-09-13 10:25:03 -0400

    Improved graceful disconnect

--- example/example.js
+++ example/example.js
@@ -3,19 +3,21 @@ const ClientManager = require('../index.js').ClientManager;
 
 const gameManager = new ClientManager();
 
-gameManager.addCommandListener('register', (client) => {
-  const playerId = Date.now().toString();
-  client.set('playerId', playerId);
+gameManager.addCommandListener('register', (client, data) => {
+  client.set('username', data.username);
 
   client.send('register', {
-    playerId: playerId
+    registered: true,
+    username: data.username,
   });
+
+  console.log(client.clientId, 'has registered as', data.username);
 });
 
 gameManager.on('clientAdded', (client) => {
-  console.log('Player connected.');
+  console.log('Player connected. Client id:', client.clientId);
 
-  client.send('joined', {
+  client.send('welcome', {
     motd: 'Never pet a dog on fire'
   });
 });
@@ -25,10 +27,6 @@ gameManager.on('clientDropped', ({ client, reason }) => {
 });
 
 const server = new GMServer(function (client) {
-  client.addCommandListener('register', () => {
-    console.log('Client is registering.');
-  });
-
   gameManager.addClient(client);
 });
 
